3af37e53ef841616d2b382423a0b5a76375e9ffe
yeqianfeng <yeqianfeng911@gmail.com>
2018-12-27 17:21:27 +0800

    Create events.py

---
+++ events.py
@@ -0,0 +1,29 @@
+class Event(object):
+    pass
+
+
+class MarketEvent(Event):
+    def __init__(self, bars):
+        self.type = 'market'
+        self.bars = bars
+
+
+class SignalEvent(Event):
+    def __init__(self, signal):
+        self.type = 'signal'
+        self.signal = signal
+
+
+class OrderEvent(Event):
+    def __init__(self, order):
+        self.type = 'order'
+        self.order = order
+
+
+class FillEvent(Event):
+    def __init__(self, symbol, price, quantity, action):
+        self.type = 'fill'
+        self.symbol = symbol
+        self.quantity = quantity
+        self.price = price
+        self.action = action
